562e6f5a90d5963ee004fcf775f02101b80ff941
Mahen Krishnakumar <mahkrish@users.noreply.github.com>
2018-05-04 01:51:09 -0700

    Update 3GHAT.sh

--- 3GHAT.sh
+++ 3GHAT.sh
@@ -54,7 +54,7 @@ sudo ./IOTBit_Install.sh
 
 
 Driver=GobiSerial
-DETECT_DRIVER=$(lsmod | grep $Driver)
+DETECT_DRIVER=$(modinfo $Driver)
 if [[ $DETECT_DRIVER = "" ]]
 then
   echo "ERROR: Driver Installation has failed."
@@ -62,26 +62,9 @@ then
   exit 1
 fi
 
-#Editing WVDIAL Configuration Files
 echo $SPACING
 echo $SPACING
-echo "Driver has been installed properly! :)"
-echo "Proceeding to customize wvidal config files."
-echo $SPACING
-echo "What APN does your mobile carrier use? (Make sure it is used for data plans.)"
-read APN
-sudo sed -i .bak "s/YOUR.SIMCARD_APN.HERE/$APN/" *wvdial*
-sudo mv /etc/wvdial.conf /etc/wvdial.conf.bak
-sudo mv wvdial.conf /etc/wvdial.conf
-sudo wvdialconf /etc/wvdial.conf
-
-# Changing permissions for S0 serial port.
-sudo usermod -a -G /dev/ttyS0 pi
-echo $SPACING
-echo $SPACING
-
-# Final message before shutdown
-echo "Driver has been installed and the configuration files have been set."
+echo "Driver has sucessfully been installed."
 echo "After reboot, please run the DialSetup.sh bash script."
 echo "Rebooting in 60 seconds."
 sleep 1m
